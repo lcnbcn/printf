edb2b85fe5a8ec65b1f97f76b441466f45ced0b5
Luciano Da Silva Mendes <lucida-s@cbr2s3.42barcelona.com>
2023-01-19 17:06:16 +0100

    printf v06 terminado sin pasar por paco

--- ft_printf.c
+++ ft_printf.c
@@ -6,26 +6,67 @@
 /*   By: lucida-s <lucida-s@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 19:09:59 by lucida-s          #+#    #+#             */
-/*   Updated: 2023/01/17 12:15:09 by lucida-s         ###   ########.fr       */
+/*   Updated: 2023/01/19 17:03:54 by lucida-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-/*int	ft_printf(char const *format, ...)
+
+int	ft_type(int count, va_list args, char c)
 {
-	
-}*/
+	if (c == '%')
+		count = ft_fmt_c(count, '%');
+	if (c == 'c')
+		count = ft_fmt_c(count, va_arg(args, char));
+	if (c == 's')
+		count = ft_fmt_s(count, va_arg(args, char *));
+	if (c == 'i' || c == 'd')
+		count = ft_fmt_id(count, va_arg(args, int));
+	if (c == 'u')
+		count = ft_fmt_uxX(count, va_arg(args, unsigned int, 1, 10));
+	if (c == 'x')
+		count = ft_fmt_uxX(count, va_arg(args, unsigned int, 2, 16);
+	if (c == 'X')
+		count = ft_fmt_uxX(count, va_arg(args, unsigned int, 3, 16);
+	if (c == 'p')
+		count = ft_fmt_p(count, va_arg(args, unsigned long int);
+	return (count);
+}
 
-int	main(void)
-
+int	ft_check(int count, va_list args, char *str)
 {
-	char	*a;
-	
-	a = "10";
-	//ft_fmt_p(0, *a);
+	int	i;
 
-	printf("%p", a);
-	return (0);
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == '%')
+		{
+			count = ft_type(count, args, str[i + 1]);
+			if (count == -1)
+				return (-1);
+			i++;
+		}
+		else
+		{
+			count = ft_fmt_c(count, str[i]);
+			if (count == -1)
+				return (-1);
+		}
+	}
+	return (count);
+}
+
+int	ft_printf(char const *str, ...)
+{
+	int 	count;
+	va_list	args;
+
+	count = 0;
+	va_start(args, str);
+	count = ft_check(count, args, str);
+	va_end(args);
+	return (count);
 }
